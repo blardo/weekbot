520d1cbb8fa80cdbfac4f16c45335582b3a01388
Ayobdon <AEXBrandon@gmail.com>
2024-06-06 20:51:43 -0400

    Ephemeral Message Poll
    Now sends a button that sends the poll as an ephemeral message so each user has a unique ballot instance

--- internal/commands/poll.go
+++ internal/commands/poll.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"log"
 	"weekbot/internal/models"
 
 	"github.com/bwmarrin/discordgo"
@@ -14,15 +15,19 @@ func HandleWeekPoll(s *discordgo.Session, m *discordgo.InteractionCreate) {
 	
 	if poll == nil {
 		println("poll is nil")
-		s.InteractionRespond(m.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
+		s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
 				Content: "No suggestions found",
-			},
 		})
 		return
 	}
 	println("poll is not nil")
+
+
+	////
+	////
+	/// Idea - Lets send a button that shows modal with all the suggestions
+	//// so we can process the information independantly of the message itself.
+	////
 	
 	options := poll.GetSelectOptions()
 	for _, option := range options {
@@ -35,34 +40,84 @@ func HandleWeekPoll(s *discordgo.Session, m *discordgo.InteractionCreate) {
 			Components: []discordgo.MessageComponent{
 				&discordgo.ActionsRow{
 					Components: []discordgo.MessageComponent{
-						discordgo.SelectMenu{
-							CustomID:    "first_choice",
-							Placeholder: "Select a week",
-							Options:     poll.GetSelectOptions(),
-						},
-					},
-				},
-				&discordgo.ActionsRow{
-					Components: []discordgo.MessageComponent{
-						discordgo.SelectMenu{
-							CustomID:    "second_choice",
-							Placeholder: "Select a week",
-							Options:     poll.GetSelectOptions(),
-						},
-					},
-				},
-				&discordgo.ActionsRow{
-					Components: []discordgo.MessageComponent{
-						discordgo.SelectMenu{
-							CustomID:    "third_choice",
-							Placeholder: "Select a week",
-							Options:     poll.GetSelectOptions(),
+						&discordgo.Button{
+							CustomID: "poll_button",
+							Label:    "Vote Here",
+							Style:    discordgo.PrimaryButton,
+							Emoji: discordgo.ComponentEmoji{
+								Name: "🗳️",
+							},
 						},
 					},
 				},
 			},
 		},
 	})
+
+
+	// click button, check if userId in array, if no show modal, select option, submit, add userId to array, add ballot to poll
+
+
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	
+		if i.Type == discordgo.InteractionMessageComponent && i.MessageComponentData().CustomID == "poll_button" {
+	
+			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Select your options",
+					Flags: discordgo.MessageFlagsEphemeral,
+					Components: []discordgo.MessageComponent{
+						&discordgo.ActionsRow{
+							Components: []discordgo.MessageComponent{
+								discordgo.SelectMenu{
+									CustomID:    "first_choice",
+									Placeholder: "Select a week",
+									Options:     poll.GetSelectOptions(),
+								},
+							},
+						},
+						&discordgo.ActionsRow{
+							Components: []discordgo.MessageComponent{
+								discordgo.SelectMenu{
+									CustomID:    "second_choice",
+									Placeholder: "Select a week",
+									Options:     poll.GetSelectOptions(),
+								},
+							},
+						},
+						&discordgo.ActionsRow{
+							Components: []discordgo.MessageComponent{
+								discordgo.SelectMenu{
+									CustomID:    "third_choice",
+									Placeholder: "Select a week",
+									Options:     poll.GetSelectOptions(),
+								},
+							},
+						},
+						&discordgo.ActionsRow{
+							Components: []discordgo.MessageComponent{
+								discordgo.Button{
+									CustomID: "submit_button",
+									Label:    "Submit",
+									Style:    discordgo.PrimaryButton,
+									Emoji: discordgo.ComponentEmoji{
+										Name: "🗳️",
+									},
+								},
+							},
+						},
+					},
+				},
+			})
+
+			if err != nil {
+				log.Printf("Error responding to interaction: %v", err)
+				return
+			}
+		}
+	})
+	// create listener for select menu
 }
 
 func HandleEndPoll(s *discordgo.Session, m *discordgo.InteractionCreate) {
@@ -79,7 +134,7 @@ func HandleEndPoll(s *discordgo.Session, m *discordgo.InteractionCreate) {
 		})
 		return
 	}
-
+	
 	poll.IsComplete = true
 	bot.DB.Save(poll)
 
@@ -89,6 +144,8 @@ func HandleEndPoll(s *discordgo.Session, m *discordgo.InteractionCreate) {
 			Content: "The poll has been ended",
 		},
 	})
+
+	// end listener for select menu
 }
 
 func GetPollCommand() *discordgo.ApplicationCommand {
@@ -108,3 +165,6 @@ func EndPollCommand() *discordgo.ApplicationCommand {
 	}
 	return pollCommand
 }
+
+
+
