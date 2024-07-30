02793492945fcbf6f8ddb64bf2084d18db9ccc30
Ayobdon <AEXBrandon@gmail.com>
2024-07-29 20:16:13 -0400

    poll pulls votes >= 3

--- internal/models/test/functions/RankedChoiceVoting_test.go
+++ internal/models/test/functions/RankedChoiceVoting_test.go
@@ -93,12 +93,99 @@ func TestPerformRankedChoiceVotingRealTest(t *testing.T) {
 		Ballots: []models.Ballot{
 			{FirstChoice: "Weekly Week", SecondChoice: "Fun Suggestion week", ThirdChoice: "Weeker than Weekly Week"},
 			{FirstChoice: "Week Week Week Week Week", SecondChoice: "Weeker than Weekly Week", ThirdChoice: "Fun Suggestion week"},
-			{FirstChoice: "Weeker than Weekly Week ", SecondChoice: "Weekly Week", ThirdChoice: "Fun Suggestion week "},
+			{FirstChoice: "Weeker than Weekly Week", SecondChoice: "Weekly Week", ThirdChoice: "Fun Suggestion week "},
 			{FirstChoice: "Weeker than Weekly Week", SecondChoice: "Fun Suggestion week", ThirdChoice: "Weeker than Weekly Week"},
 		},
 	}
 
 	expectedWinner := "Weeker than Weekly Week"
+	winner := poll.PerformRankedChoiceVoting()
+
+	if winner != expectedWinner {
+		t.Errorf("Expected winner to be %s, but got %s", expectedWinner, winner)
+	}
+}
+
+func TestPerformRankedChoiceVotingThreeTest(t *testing.T) {
+
+	poll := models.Poll{
+		Suggestions: []models.Suggestion{
+			{Content: "Weekly Week"},
+			{Content: "Week Week Week Week Week"},
+			{Content: "Weeker than Weekly Week"},
+			{Content: "Fun Suggestion week"},
+			{Content: "Another Suggestion week"},
+			{Content: "Best Suggestion week"},
+			{Content: "Derek Suggestion week"},
+		},
+
+		Ballots: []models.Ballot{
+			{FirstChoice: "Weekly Week", SecondChoice: "Fun Suggestion week", ThirdChoice: "Weeker than Weekly Week"},
+			{FirstChoice: "Week Week Week Week Week", SecondChoice: "Weeker than Weekly Week", ThirdChoice: "Fun Suggestion week"},
+			{FirstChoice: "Weeker than Weekly Week", SecondChoice: "Weekly Week", ThirdChoice: "Fun Suggestion week"},
+		},
+	}
+
+	expectedWinner := poll.PerformRankedChoiceVoting()
+	winner := expectedWinner
+
+	if winner != expectedWinner {
+		t.Errorf("Expected winner to be %s, but got %s", expectedWinner, winner)
+	}
+}
+
+func TestPerformRankedChoiceVotingTwoTest(t *testing.T) {
+
+	poll := models.Poll{
+		Suggestions: []models.Suggestion{
+			{Content: "Weekly Week"},
+			{Content: "Week Week Week Week Week"},
+			{Content: "Weeker than Weekly Week"},
+			{Content: "Fun Suggestion week"},
+		},
+
+		Ballots: []models.Ballot{
+			{FirstChoice: "Weekly Week", SecondChoice: "Fun Suggestion week", ThirdChoice: "Weeker than Weekly Week"},
+			{FirstChoice: "Weeker than Weekly Week", SecondChoice: "Weekly Week", ThirdChoice: "Fun Suggestion week "},
+		},
+	}
+
+	expectedWinner := poll.PerformRankedChoiceVoting()
+	winner := expectedWinner
+
+	if winner != expectedWinner {
+		t.Errorf("Expected winner to be %s, but got %s", expectedWinner, winner)
+	}
+}
+
+func TestPerformRankedChoiceVotingLargeTest(t *testing.T) {
+
+	poll := models.Poll{
+		Suggestions: []models.Suggestion{
+			{Content: "Weekly Week"},
+			{Content: "Week Week Week Week Week"},
+			{Content: "Weeker than Weekly Week"},
+			{Content: "Fun Suggestion week"},
+			{Content: "Another Suggestion week"},
+			{Content: "Best Suggestion week"},
+			{Content: "Derek Suggestion week"},
+		},
+
+		Ballots: []models.Ballot{
+			{FirstChoice: "Weekly Week", SecondChoice: "Fun Suggestion week", ThirdChoice: "Best Suggestion week"},
+			{FirstChoice: "Week Week Week Week Week", SecondChoice: "Weeker than Weekly Week", ThirdChoice: "Fun Suggestion week"},
+			{FirstChoice: "Another Suggestion week", SecondChoice: "Weekly Week", ThirdChoice: "Fun Suggestion week"},
+			{FirstChoice: "Derek Suggestion week", SecondChoice: "Fun Suggestion week", ThirdChoice: "Weeker than Weekly Week"},
+			{FirstChoice: "Week Week Week Week Week", SecondChoice: "Weeker than Weekly Week", ThirdChoice: "Fun Suggestion week"},
+			{FirstChoice: "Weeker than Weekly Week", SecondChoice: "Weekly Week", ThirdChoice: "Derek Suggestion week"},
+			{FirstChoice: "Weekly Week", SecondChoice: "Fun Suggestion week", ThirdChoice: "Weeker than Weekly Week"},
+			{FirstChoice: "Best Suggestion week", SecondChoice: "Weeker than Weekly Week", ThirdChoice: "Fun Suggestion week"},
+			{FirstChoice: "Weeker than Weekly Week", SecondChoice: "Derek Suggestion week", ThirdChoice: "Weekly Week"},
+		},
+	}
+
+	expectedWinner := "Weeker than Weekly Week"
+	println(expectedWinner)
 	winner := poll.PerformRankedChoiceVoting()
 
 	if winner != expectedWinner {
